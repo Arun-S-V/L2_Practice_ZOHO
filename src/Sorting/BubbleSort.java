e263d12e842d1902837a271c908309277d9aa09f
ARUN S V <69088220+Arun-S-V@users.noreply.github.com>
2021-09-18 22:25:08 +0530

    initial Commit

---
+++ src/Sorting/BubbleSort.java
@@ -0,0 +1,13 @@
+public class BubbleSort {  
+    static void bubbleSort(int[] arr,n){
+		for(i = 0; i < n; i++){    
+			for(j = i+1; j < n; j++){    
+				if(a[j] < a[i])    {    
+					temp = a[i];    
+					a[i] = a[j];    
+					a[j] = temp;     
+                }    
+            }     
+        }       
+	}
+}
