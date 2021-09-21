4a85a976b92b351355bff482f68174f5f7642368
inc2 <svarun24200@gmail.com>
2021-09-21 19:22:29 +0530

    initial commit

--- src/Numerical/LargestPossiblePrimeNumber.java
+++ src/Numerical/LargestPossiblePrimeNumber.java
@@ -1,7 +1,20 @@
 package Numerical;
 
+import java.util.Scanner;
+
 public class LargestPossiblePrimeNumber {
     public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        int numArray[] = new int[n];
+        for(int i = 0;i<n;i++){
+            numArray[i] = scan.nextInt();
+        }
+        String s1 = "";
+        for(int i =0;i<numArray.length;i++){
+            s1 = s1 + numArray[i];
+        }
+        int num = Integer.parseInt(s1);
 
     }
 }
