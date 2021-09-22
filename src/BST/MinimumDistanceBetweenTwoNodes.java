23aa66343f4cfa4f5cb4ec67a5dac9a81c7fa8f7
inc2 <svarun24200@gmail.com>
2021-09-22 10:20:04 +0530

    initial commit

---
+++ src/BST/MinimumDistanceBetweenTwoNodes.java
@@ -0,0 +1,36 @@
+package BST;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MinimumDistanceBetweenTwoNodes {
+    Node root;
+    class Node{
+        int data;
+        Node left;
+        Node right;
+        Node(int data){
+            this.data = data;
+            left = null;
+            right = null;
+        }
+    }
+    MinimumDistanceBetweenTwoNodes(){
+        root = null;
+    }
+    public int minDiffInBST(Node root) {
+        List<Integer> list = new ArrayList<>();
+        inOrder(root, list);
+        int min = Integer.MAX_VALUE;
+        for(int i = 0; i < list.size()-1; i++)
+            if(min > list.get(i+1)-list.get(i))
+                min = list.get(i+1)-list.get(i);
+        return min;
+    }
+    public void inOrder(Node t, List<Integer> l){
+        if(t == null) return;
+        inOrder(t.left, l);
+        l.add(t.data);
+        inOrder(t.right, l);
+    }
+}
